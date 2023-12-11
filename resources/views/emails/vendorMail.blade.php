dde74a4ab1c24b75533f0410962d27e8ab4e3795
sogatanco <adeliapucha46@gmail.com>
2023-12-11 14:27:02 +0700

    update link

--- resources/views/emails/vendorMail.blade.php
+++ resources/views/emails/vendorMail.blade.php
@@ -196,7 +196,7 @@
           <tr>
             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
               <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
-              <p style="margin: 0;"><a href="https://blogdesire.com" target="_blank">https://blogdesire.com/xxx-xxx-xxxx</a></p>
+              <p style="margin: 0;"><a href="<?php echo $mailData['link']?>" target="_blank"><?php echo $mailData['link']?></a></p>
             </td>
           </tr>
           <!-- end copy -->
