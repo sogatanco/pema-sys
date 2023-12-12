6671513c7db09a2e60987550ca0ca1724d200cfd
sogatanco <adeliapucha46@gmail.com>
2023-12-12 17:07:03 +0700

    dgsg

--- app/Http/Controllers/Auth2Controller.php
+++ app/Http/Controllers/Auth2Controller.php
@@ -127,8 +127,8 @@ class Auth2Controller extends Controller
         $digits = 10;
         $uniq = base64_encode((rand(pow(10, $digits - 1), pow(10, $digits) - 1)) . ($id + 45) . '-' . strtotime(now()));
         $mailData = [
-            'link' => Config::get('app.url') . 'api/auth2/verif/' . $uniq,
-            'company_name' => $per['bentuk_usaha'] . ' ' . $per['nama_perusahaan']
+            'link' => Config::get('app.url') . 'api/auth2/verif/' . $uniq
+            // 'company_name' => $per['bentuk_usaha'] . ' ' . $per['nama_perusahaan']
         ];
         if (Mail::to($per['email'])->send(new VendorMail($mailData))) {
             return view('emails.sentEmail')->with('email', $per['email']);
