0b9cc6877ed055f4c9d239b29058f8a5d98991b0
sogatanco <adeliapucha46@gmail.com>
2023-12-13 14:45:13 +0700

    dsgsdg

--- routes/vapi.php
+++ routes/vapi.php
@@ -23,4 +23,5 @@ Route::controller(PerusahaanController::class)->group(function() {
 Route::controller(JajaranController::class)->group(function() {
     Route::post('jajaran/store', 'store');
     Route::post('jajaran/edit/{id}', 'edit');
+    Route::get('jajaran/my', 'myDirek');
 });
