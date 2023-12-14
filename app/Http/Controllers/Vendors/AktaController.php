2cdcf8bfad7bbccce2a688394ce81146ce197f3c
sogatanco <adeliapucha46@gmail.com>
2023-12-14 10:39:42 +0700

    wahyu

--- app/Http/Controllers/Vendors/AktaController.php
+++ app/Http/Controllers/Vendors/AktaController.php
@@ -15,15 +15,18 @@ class AktaController extends Controller
     public function store(Request $request)
     {
         $file = base64_decode($request->file, true);
-        $filename = 'akta/' . time() . '.pdf'; 
+        $filename = 'akta/' . time() . '.pdf';
         if (Storage::disk('public_vendor')->put($filename, $file)) {
-            return new PostResource(true, 'dsgsdg', []);
+            $akt = new Akta();
+            $akt->id_perusahaan = ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id;
+            $akt->no_akta = $request->no_akta;
+            $akt->tgl_terbit = $request->tgl_terbit;
+            $akt->nama_notaris = $request->nama_notaris;
+            $akt->file_akta = $filename;
+            if($akt->save()){
+                return new PostResource(true, 'dsgsdg', []);
+            }else
+           
         }
-        // $akt=new Akta();
-        // $akt->id_perusahaan=ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id;
-        // $akt->no_akta=$request->no_akta;
-        // $akt->tgl_terbit=$request->tgl_terbit;
-        // $akt->nama_notaris=$request->nama_notaris;
-        // $akt->file_akta=$filename;
     }
 }
