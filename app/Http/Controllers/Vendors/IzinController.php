712536230a58b49cf5039f0bbab90be76a90e559
sogatanco <adeliapucha46@gmail.com>
2023-12-15 15:16:20 +0700

    ds

--- app/Http/Controllers/Vendors/IzinController.php
+++ app/Http/Controllers/Vendors/IzinController.php
@@ -56,12 +56,14 @@ class IzinController extends Controller
         $izin = Izin::where('id_izin', $id)->first();
         if ($izin->perusahaan_id == ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id) {
             if (Storage::exists('vendor_file/' . $izin->file_izin)) {
-                Storage::delete('vendor_file/' . $izin->file_izin);
+                return new PostResource(true, 'ada', []);
+                // Storage::delete('vendor_file/' . $izin->file_izin);
             }
+            return new PostResource(true, 'tdidak sada', []);
 
-            if ($izin->delete()) {
-                return new PostResource(true, 'Deleted Succesfully', []);
-            }
+            // if ($izin->delete()) {
+            //     return new PostResource(true, 'Deleted Succesfully', []);
+            // }
         } else {
             return new PostResource(false, 'Not Permitted', []);
         }
