bae0def0f6a5dbf2be4165f4ad3f30daaa5e19d2
unknown <hello.safrian@gmail.com>
2023-12-27 10:33:41 +0700

    update masterkbli model

--- app/Models/Vendor/MasterKbli.php
+++ app/Models/Vendor/MasterKbli.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class MasterKbli extends Model
 {
     use HasFactory;
+    protected $connection ='mysql2';
     protected $table = 'master_kbli';
+    protected $primaryKey = 'id_kbli';
 }
