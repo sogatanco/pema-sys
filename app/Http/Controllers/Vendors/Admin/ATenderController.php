921715293ab97e29b689cc5f43bacd9338790cf6
unknown <hello.safrian@gmail.com>
2024-01-02 17:20:37 +0700

    update Atender controller

--- app/Http/Controllers/Vendors/Admin/ATenderController.php
+++ app/Http/Controllers/Vendors/Admin/ATenderController.php
@@ -12,10 +12,29 @@ class ATenderController extends Controller
 {
     function store(Request $request)
     {
-        return response()->json([
-            "status" => "ok",
-            "data" => $request->all()
-        ], 200);
+        $t = new Tender();
+        $t->user_id = $request->user_id;
+        $t->pilihan_tender = $request->pilihan_tender;
+        $t->metode_pengadaan = $request->metode_pengadaan;
+        $t->sistem_kualifikasi = $request->sistem_kualifikasi;
+        $t->nama_tender = $request->nama_tender;
+        $t->lokasi = $request->lokasi;
+        $t->tgl_pendaftaran = $request->tgl_pendaftaran;
+        $t->batas_pendafaran = $request->batas_pendafaran;
+        // $t->masa_sanggah = $request->masa_sanggah;
+        // $t->tgl_masa_sanggah = $request->tgl_masa_sanggah;
+        $t->jenis_pengadaan = $request->jenis_pengadaan;
+        $t->hpd = $request->hps;
+        $t->kbli = $request->kbli;
+        $t->centang_dok_wajib = $request->centang_dok_wajib;
+        $t->dok_tender = $dok_tender;
+        $t->dok_deskripsi_tender = $dok_deskripsi_tender;
+
+        if($t->save()){
+            return new PostResource(true, 'Tender Inserted !', []);
+        }else{
+            return new PostResource(false, 'Failed Tender Insert !', []);
+        }
 
         $file_dok_tender = base64_decode($request->dok_tender, true);
         $file_dok_deskripsi_tender = base64_decode($request->dok_deskripsi_tender, true);
@@ -31,11 +50,12 @@ class ATenderController extends Controller
             $t->sistem_kualifikasi = $request->sistem_kualifikasi;
             $t->nama_tender = $request->nama_tender;
             $t->lokasi = $request->lokasi;
+            $t->tgl_pendaftaran = $request->tgl_pendaftaran;
             $t->batas_pendafaran = $request->batas_pendafaran;
-            $t->masa_sanggah = $request->masa_sanggah;
-            $t->tgl_masa_sanggah = $request->tgl_masa_sanggah;
-            $t->tipe_penyedia = $request->tipe_penyedia;
-            $t->bidang_usaha = $request->bidang_usaha;
+            // $t->masa_sanggah = $request->masa_sanggah;
+            // $t->tgl_masa_sanggah = $request->tgl_masa_sanggah;
+            $t->jenis_pengadaan = $request->jenis_pengadaan;
+            $t->hpd = $request->hps;
             $t->kbli = $request->kbli;
             $t->centang_dok_wajib = $request->centang_dok_wajib;
             $t->dok_tender = $dok_tender;
