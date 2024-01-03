be6277d09dee715147c221e14571338242fdcb8b
sogatanco <adeliapucha46@gmail.com>
2024-01-03 15:55:40 +0700

    dd

--- app/Http/Controllers/Vendors/Admin/ATenderController.php
+++ app/Http/Controllers/Vendors/Admin/ATenderController.php
@@ -12,42 +12,44 @@ class ATenderController extends Controller
 {
     function store(Request $request)
     {
+
         $file_dok_tender = base64_decode($request->dok_tender, true);
         $file_dok_deskripsi_tender = base64_decode($request->dok_deskripsi_tender, true);
 
-        $dok_tender = 'dok_tender/' . time().'.pdf';
-        $dok_deskripsi_tender = 'dok_deskripsi_tender/' . time(). '.pdf';
+        $dok_tender = 'dok_tender.pdf';
+        $dok_deskripsi_tender = 'dok_deskripsi_tender.pdf';
 
-        if (Storage::disk('public_vendor')->put($dok_tender, $file_dok_tender) && Storage::disk('public_vendor')->put($dok_deskripsi_tender, $file_dok_deskripsi_tender)) {
-            $t = new Tender();
-            $t->user_id = $request->user_id;
-            $t->pilihan_tender = $request->pilihan_tender;
-            $t->metode_pengadaan = $request->metode_pengadaan;
-            $t->sistem_kualifikasi = $request->sistem_kualifikasi;
-            $t->nama_tender = $request->nama_tender;
-            $t->lokasi = $request->lokasi;
-            $t->tgl_pendaftaran = $request->tgl_pendaftaran;
-            $t->batas_pendaftaran = $request->batas_pendaftaran;
-            // $t->masa_sanggah = $request->masa_sanggah;
-            // $t->tgl_masa_sanggah = $request->tgl_masa_sanggah;
-            $t->jenis_pengadaan = $request->jenis_pengadaan;
-            $t->hps = $request->hps;
-            $t->kbli = $request->kbli;
-            $t->centang_dok_wajib = json_encode($request->centang_dok_wajib);
-            $t->dok_tender = $request->dok_tender;
-            $t->dok_deskripsi_tender = $request->dok_deskripsi_tender;
 
-            if($t->save()){
-                return new PostResource(true, 'Tender Inserted !', []);
-            }else{
-                return new PostResource(false, 'Failed Tender Insert !', []);
-            }
+        $t = new Tender();
+        $t->user_id = $request->user_id;
+        $t->pilihan_tender = $request->pilihan_tender;
+        $t->metode_pengadaan = $request->metode_pengadaan;
+        $t->sistem_kualifikasi = $request->sistem_kualifikasi;
+        $t->nama_tender = $request->nama_tender;
+        $t->lokasi = $request->lokasi;
+        $t->tgl_pendaftaran = $request->tgl_pendaftaran;
+        $t->batas_pendaftaran = $request->batas_pendaftaran;
+        // $t->masa_sanggah = $request->masa_sanggah;
+        // $t->tgl_masa_sanggah = $request->tgl_masa_sanggah;
+        $t->jenis_pengadaan = $request->jenis_pengadaan;
+        $t->hps = $request->hps;
+        $t->kbli = $request->kbli;
+        $t->centang_dok_wajib = json_encode($request->centang_dok_wajib);
+        $t->dok_tender = $dok_tender;
+        $t->dok_deskripsi_tender = $dok_deskripsi_tender;
+
+        if ($t->save()) {
+            Storage::disk('public_vendor')->put($t->id . '/' . $dok_tender, $file_dok_tender);
+            Storage::disk('public_vendor')->put($t->id . '/' . $dok_deskripsi_tender, $file_dok_deskripsi_tender);
+            return new PostResource(true, 'Tender Inserted !', []);
+        } else {
+            return new PostResource(false, 'Failed Tender Insert !', []);
         }
-        return new PostResource(false, 'Failed Tender Insert !', []);
     }
 
-    function index(){
-        $data=Tender::get();
+    function index()
+    {
+        $data = Tender::get();
         return new PostResource(true, 'List Tender', $data);
     }
 }
