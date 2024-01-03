6d4feb35d81fb22382b869e2b147c43829a1dabc
unknown <hello.safrian@gmail.com>
2024-01-03 15:45:33 +0700

    update atender controller

--- app/Http/Controllers/Vendors/Admin/ATenderController.php
+++ app/Http/Controllers/Vendors/Admin/ATenderController.php
@@ -12,30 +12,6 @@ class ATenderController extends Controller
 {
     function store(Request $request)
     {
-        $t = new Tender();
-        $t->user_id = $request->user_id;
-        $t->pilihan_tender = $request->pilihan_tender;
-        $t->metode_pengadaan = $request->metode_pengadaan;
-        $t->sistem_kualifikasi = $request->sistem_kualifikasi;
-        $t->nama_tender = $request->nama_tender;
-        $t->lokasi = $request->lokasi;
-        $t->tgl_pendaftaran = $request->tgl_pendaftaran;
-        $t->batas_pendaftaran = $request->batas_pendaftaran;
-        // $t->masa_sanggah = $request->masa_sanggah;
-        // $t->tgl_masa_sanggah = $request->tgl_masa_sanggah;
-        $t->jenis_pengadaan = $request->jenis_pengadaan;
-        $t->hps = $request->hps;
-        $t->kbli = $request->kbli;
-        $t->centang_dok_wajib = json_encode($request->centang_dok_wajib);
-        $t->dok_tender = $request->dok_tender;
-        $t->dok_deskripsi_tender = $request->dok_deskripsi_tender;
-
-        if($t->save()){
-            return new PostResource(true, 'Tender Inserted !', []);
-        }else{
-            return new PostResource(false, 'Failed Tender Insert !', []);
-        }
-
         $file_dok_tender = base64_decode($request->dok_tender, true);
         $file_dok_deskripsi_tender = base64_decode($request->dok_deskripsi_tender, true);
 
@@ -55,16 +31,17 @@ class ATenderController extends Controller
             // $t->masa_sanggah = $request->masa_sanggah;
             // $t->tgl_masa_sanggah = $request->tgl_masa_sanggah;
             $t->jenis_pengadaan = $request->jenis_pengadaan;
-            $t->hpd = $request->hps;
+            $t->hps = $request->hps;
             $t->kbli = $request->kbli;
             $t->centang_dok_wajib = json_encode($request->centang_dok_wajib);
-            $t->dok_tender = $dok_tender;
-            $t->dok_deskripsi_tender = $dok_deskripsi_tender;
+            $t->dok_tender = $request->dok_tender;
+            $t->dok_deskripsi_tender = $request->dok_deskripsi_tender;
 
             if($t->save()){
                 return new PostResource(true, 'Tender Inserted !', []);
+            }else{
+                return new PostResource(false, 'Failed Tender Insert !', []);
             }
-            return new PostResource(false, 'Failed Tender Insert !', []);
         }
         return new PostResource(false, 'Failed Tender Insert !', []);
     }
